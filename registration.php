2ebde34b4d50a3812329c0b52a54c99e3d297779
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-12-16 18:22:59 +0100

    + Added last name verification

--- registration.php
+++ registration.php
@@ -16,6 +16,16 @@
 	
 	if (!ctype_alnum($firstName)) {
 		$correctData = false;
-		$_SESSION['e-form-first-name'] = "First name can contain only alphanumerc characters"; 
+		$_SESSION['e-form-first-name'] = "First name can contain only alphanumeric characters"; 
+	}
+	
+	if ((strlen($lastName) < 2) || (strlen($lastName) > 50)) {
+		$correctData = false;
+		$_SESSION['e-form-last-name'] = "Last name must contain from 2 to 50 characters";
+	}
+	
+	if (!ctype_alnum($lastName)) {
+		$correctData = false;
+		$_SESSION['e-form-last-name'] = "Last name can contain only alphanumeric characters"; 
 	}
 ?>
