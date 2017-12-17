ac6fc60fab2f2840ca0c7b2d6e974f5e648b84d9
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-12-17 20:16:27 +0100

    + Added insert user to database

--- registration.php
+++ registration.php
@@ -130,12 +130,21 @@
           header("Location: index.php");
           exit();
         }
+        
+        $password_hash = password_hash($password1, PASSWORD_DEFAULT);
+        if ($connection->query("INSERT INTO users(firstname, lastname, nickname, email, password_hash) VALUES('$firstName', '$lastName', '$nickname', '$email', '$password_hash')")) {
+          $_SESSION['registration-result'] = '<div class="registration-success">Congratulations you have register! Now you can login.</div>';
+        } else {
+          $connection->close();
+          throw new Exception($polaczenie->error);
+        }
                 
         $conncection->close();
+        header('Location: index.php');
     }
   }
   catch (Exception $e) {
-    $_SESSION['server-error'] = '<div class="server-error">It looks like we have a server error. Please try to register later</div>';
+    $_SESSION['registration-result'] = '<div class="server-error">It looks like we have a server error. Please try to register later</div>';
     echo '</br> Error info: ' . $e . '<br/><br/>';
   }
   
