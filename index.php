53e77d2defea10d900674c981bacfc65a3fb85be
Gaspar Kwiecien 195868 <195868@edu.p.lodz.pl>
2017-12-19 00:09:33 +0100

    + Fixed indents

--- index.php
+++ index.php
@@ -12,17 +12,18 @@
     <title>E-sport</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSS -->
     <link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="resources/css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=latin-ext" rel="stylesheet">
-	<link rel="stylesheet" href="resources/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="resources/font-awesome/css/font-awesome.min.css">
   
-	<script src='https://www.google.com/recaptcha/api.js'></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
   </head>
+  
   <body>
 	
 	<!-- Header -->
@@ -218,8 +219,8 @@
 		</div>
 	</footer>
 	
-    <!-- Optional JavaScript -->
-    <!-- jQuery, Bootstrap JS -->
+  <!-- Optional JavaScript -->
+  <!-- jQuery, Bootstrap JS -->
 	<script src="resources/js/jquery-3.2.1.min.js"></sript>
 	<script src="resources/bootstrap/js/bootstrap.min.js"></script>	
 	
