9a754d3475ad2c801c4ffe3580ec3bfce5ddc2c1
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-12-18 01:14:02 +0100

    + Added password verification

--- login.php
+++ login.php
@@ -18,29 +18,38 @@
   try {
     $connection = new mysqli($host, $db_user, $db_password, $db_name);
     
-    if ($connection->connect_errno!=0) {
+    if ($connection->connect_errno != 0) {
       throw new Exception(mysqli_connect_errno());
     }
     else {
       
       $email    = htmlentities($email, ENT_QUOTES, "UTF-8");
       $password = htmlentities($password, ENT_QUOTES, "UTF-8");
+      echo $email;
       
-      
-      if ($result = $connection->query(sprintf("SELECT * FROM users WHERE email='%s'",
-      $connection->real_escape_string($email)))) {
+      if ($result = $connection->query(sprintf("SELECT * FROM users WHERE email='$email';"))) {
         $rows = $result->num_rows;
         if ($rows > 0) {
           $row = $result->fetch_assoc();
-          
-          echo $row['name'] . " " . $row['surname'];
+          if (password_verify($password, $row['Password_Hash'])) {
+            $_SESSION['logedin'] = true;
+            $_SESSION['id'] = $rows['Id'];
+            $_SESSION['nickname'] = $rows['Nickname'];
+            $result->free_result();
+            echo "Your are loged in " . $_SESSION['nickname'];
+            //header('Location: main.php');
+          }
+          else {
+            $_SESSION['user-not-found'] = "Invalid user or password";
+            header('Location: index.php');            
+          }
         }
-      }
-           
+      }          
       $connection->close();
     }
   } catch (Exception $e) {
-    echo $e;
+    $_SESSION['registration-result'] = '<div style="color: #ff1a1a; border-color: #ff1a1a;" class="server-error">It looks like we have some problems. Please try to login later</div>';
+    header('Location: index.php');
   }
   
 ?>
