4591436df58b1a755ad020d620d3b67484a2ce58
Gaspar Kwiecien 195868 <195868@edu.p.lodz.pl>
2017-12-31 00:44:20 +0100

    + Added data to main array

--- maindata.php
+++ maindata.php
@@ -50,6 +50,14 @@
          $result->free();
       }
       
+      if ($result = $connection->query("SELECT t.Name AS t_name FROM users AS u INNER JOIN teams_users AS tu ON tu.id_users=u.id INNER JOIN teams AS t ON t.id=tu.id_teams WHERE u.id=$id AND tu.ustatus='invited'")) {
+         while ($row = $result->fetch_assoc()) {
+           $team = team_arrays($row['t_name'], 'Invited', 'Accept Decline');
+           array_push($teams, $team);
+         }
+         $result->free();
+      }
+      
       /*if ($result = $connection->query("SELECT l.Name AS l_name FROM teams AS t LEFT JOIN teams_users AS tu ON t.id = tu.id_teams INNER JOIN teams_leagues AS tl ON t.id = tl.id_Team INNER JOIN leagues AS l ON l.id = tl.id_league WHERE t.id_captain=$id OR tu.id_users=$id;")) {
          while ($row = $result->fetch_assoc()) {
           array_push($leagues_member, $row);
