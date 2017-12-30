250d66ede3c33e1beb419524645231c7ecd84a54
Gaspar Kwiecien 195868 <195868@edu.p.lodz.pl>
2017-12-31 00:16:55 +0100

    + Fixed problem with index

--- maindata.php
+++ maindata.php
@@ -44,7 +44,7 @@
       
       if ($result = $connection->query("SELECT t.Name AS t_name FROM users AS u INNER JOIN teams_users AS tu ON tu.id_users=u.id INNER JOIN teams AS t ON t.id=tu.id_teams WHERE u.id=$id AND tu.ustatus='member'")) {
          while ($row = $result->fetch_assoc()) {
-           $team = team_arrays($row['t.Name'], 'Member', 'Leave');
+           $team = team_arrays($row['t_name'], 'Member', 'Leave');
            array_push($teams, $team);
          }
          $result->free();
