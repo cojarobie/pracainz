dab0b17a93d3906ee0899a3521ab753ac5e18895
Gaspar Kwiecien 195868 <195868@edu.p.lodz.pl>
2018-01-05 00:08:02 +0100

    + Added scroll function

--- main.php
+++ main.php
@@ -164,7 +164,8 @@
     <!-- Optional JavaScript -->
     <!-- jQuery, Bootstrap JS -->
     <script src="resources/js/jquery-3.2.1.min.js"></sript>
-    <script src="resources/bootstrap/js/bootstrap.min.js"></script>	
+    <script src="resources/bootstrap/js/bootstrap.min.js"></script>
+    <script src="resources/js/jquery.scrollTo.min.js"></script>
     
     <script>
       function logout() {
